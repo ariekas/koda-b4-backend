5b0efabb4a50ee07d6da34870ce822feb271a27c
ariekas <ariariekas@gmail.com>
2025-11-11 14:14:59 +0700

    refactor : update redis url and password redis put in env

--- lib/config/redis.go
+++ lib/config/redis.go
@@ -1,11 +1,19 @@
 package config
 
-import "github.com/redis/go-redis/v9"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+)
 
 func Redis() *redis.Client {
+	godotenv.Load()
+	redisUrl := os.Getenv("REDIS_URL")
+	password := os.Getenv("REDIS_PASSWORD")
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-		Password: "",
+		Addr: redisUrl,
+		Password: password,
 		DB: 0,
 	})
 
