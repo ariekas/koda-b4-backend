824f9316fbc13500a4a71c4524c06b7f94ea467e
ariekas <ariariekas@gmail.com>
2025-11-19 15:38:03 +0700

    fix: resolt typo name in cros

--- lib/middelware/cross.go
+++ lib/middelware/cross.go
@@ -6,10 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CrossMiddelware(ctx *gin.Context){
+func CrossMiddelware(ctx *gin.Context) {
 	url := config.ReadEnvUrl()
-
-	ctx.Header("AAccess-Control-Allow-Origin", url)
+	ctx.Header("Access-Control-Allow-Origin", url)
 	ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 }
