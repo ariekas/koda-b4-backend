ee7b6dec266648db57486ab37e194a82b9990239
ariekas <ariariekas@gmail.com>
2025-11-23 16:19:32 +0700

    refactor : update routes product

--- routes/admin/products.go
+++ routes/admin/products.go
@@ -1,6 +1,5 @@
 package admin
 
-
 import (
 	"back-end-coffeShop/controller"
 
@@ -8,15 +7,16 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func ProductRoutes(r *gin.RouterGroup, pool *pgxpool.Pool){
+func ProductRoutes(r *gin.RouterGroup, pool *pgxpool.Pool) {
 	productController := controller.ProductController{Pool: pool}
 
 	products := r.Group("/products")
 	{
-		products.GET("/",  productController.GetProducts)
-		products.POST("/",  productController.CreateProduct)
-		products.PATCH("/:id",  productController.EditProduct)
-		products.DELETE("/:id",  productController.DeleteProduct)
+
+		products.GET("", productController.GetProducts)
+		products.POST("", productController.CreateProduct)
+		products.PATCH("/:id", productController.EditProduct)
+		products.DELETE("/:id", productController.DeleteProduct)
 		products.POST("/image/:id", productController.CreateImageProduct)
 		products.GET("/images", productController.GetAllImageProduct)
 		products.DELETE("/image/:id", productController.DeleteImageProduct)
