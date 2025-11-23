e794720aab0eb161f927a9065cd069c70bf4820d
ariekas <ariariekas@gmail.com>
2025-11-24 01:29:08 +0700

    feat: update cross

--- lib/middelware/cross.go
+++ lib/middelware/cross.go
@@ -13,6 +13,8 @@ func CrossMiddelware() gin.HandlerFunc {
 		AllowOrigins:     []string{"http://localhost:5173", url}, 
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
 	}
 
 	return cors.New(config)
